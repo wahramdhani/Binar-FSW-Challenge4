8c74865304373145f4976ec38db91604e22f41c0
wahramdhani <wahramdhani@gmail.com>
2022-01-24 13:57:28 +0700

    fix: adding hyperlink

---
+++ sript.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    $('.carousel').carousel({
+  interval: 1800
+})
+});
+
+// document.getElementsByClassName("btn-warning").onclick = function () {
+//   windows.location.href = "game.html";
+// }
